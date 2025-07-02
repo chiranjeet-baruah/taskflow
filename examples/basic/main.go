d563de2577b558deeadfe7bcb654403f06489bbc
chiranjeet.baruah <chiranjeet.baruah@truecaller.com>
2025-07-02 19:12:00 +0530

    Test 2

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -7,4 +7,5 @@ import (
 func main() {
 	fmt.Printf("Starting the application...\n")
 	fmt.Printf("This is a basic example of a Go application.\n")
+	fmt.Printf("You can extend this application with more features as needed.\n")
 }
