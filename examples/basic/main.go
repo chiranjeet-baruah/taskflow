550ade4630baf8f754b3cddfc4b529d9db607313
Chiranjeet Baruah <chiranjeetbaruah@yahoo.in>
2025-07-02 19:53:08 +0530

    Set up GitHub code scanning

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -6,4 +6,6 @@ import (
 
 func main() {
 	fmt.Printf("Starting the application...\n")
+	fmt.Printf("This is a basic example of a Go application.\n")
+	fmt.Printf("You can extend this application with more features as needed.\n")
 }
